b108efaacfaee05499d310d63765f331131d4a7b
Mattias Andrée <maandree@operamail.com>
2014-06-15 17:47:40 +0200

    update license
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- src/epgrep.c
+++ src/epgrep.c
@@ -1,23 +1,26 @@
 /*
  * epgrep/epkill — Utilities to filter the process table
  * 
- * Copyright (C) 2000        Kjetil Torgrim Homme (kjetilho@ifi.uio.no)
- * Copyright (C) 2002, 2006  Albert Cahalan
- * Copyright (C) 2012        Roberto Polli (rpolli@babel.it)
+ * epgrep/epkill under the epkill project:
+ *   Copyright © 2014        Mattias Andrée (maandree@member.fsf.org)
  * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * pgrep/pkill under the procps-ng project:
+ *   Copyright © 2000        Kjetil Torgrim Homme (kjetilho@ifi.uio.no)
+ *   Copyright © 2002, 2006  Albert Cahalan
+ *   Copyright © 2012        Roberto Polli (rpolli@babel.it)
  * 
- * This program is distributed in the hope that it will be useful,
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include <limits.h>
 #include <stdio.h>
